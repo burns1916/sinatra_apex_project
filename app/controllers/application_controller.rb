968b79206b0c0626f283cc2a2199b9f31615df3a
Ryan Burns <ryankburns@gmail.com>
2020-12-27 10:28:38 -0800

    editted application controller and user model for session id and secure password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
 require './config/environment'
+require './app/models/user'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :"/index"
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user 
+    end
+
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  end
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     has_many :stats
     has_many :legends, through: :stats
     has_secure_password
+    
 end
