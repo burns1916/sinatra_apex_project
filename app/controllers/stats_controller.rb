e50f1fcf9a356f5ccd3e07c22af96bb5ac5f0de7
Ryan Burns <ryankburns@gmail.com>
2020-12-27 19:33:04 -0800

    created stats new route and form

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,3 +1,26 @@
 class StatsController < ApplicationController
 
+    get '/stats' do
+        @stats = Stats.all
+        erb :"/stats/index"
+    end
+
+    get '/stats/new' do
+        @legends = Legend.all
+        erb :"/stats/new"
+    end
+
+    post '/stats' do
+        @stat = Stat.create(:wins => params[:wins], :kills => params[:kills])
+
+        legend_selection = params[:stat][:legends]
+        legend_selection.each do |legend|
+            @stat.legends << Legends.find(legend)
+        end
+
+        @stat.save
+    end
+
+
+
 end
