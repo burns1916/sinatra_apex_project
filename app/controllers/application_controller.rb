59220b08f734fc242ce4a0f37f42af9e8bd1e6bd
Ryan Burns <ryankburns@gmail.com>
2020-12-27 17:50:20 -0800

    added redirect if not logged in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,12 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
+    
+    def redirect_if_not_logged_in
+        if !logged_in?
+          redirect "/login?error=You have to be logged in to do that"
+        end
+      end
 
     def logged_in?
       !!current_user 
