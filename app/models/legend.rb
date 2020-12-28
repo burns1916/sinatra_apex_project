63597348d212b22bee7e3a7c841ef0b06b45605c
Ryan Burns <ryankburns@gmail.com>
2020-12-27 17:41:42 -0800

    added access to slug methods

--- app/models/legend.rb
+++ app/models/legend.rb
@@ -1,6 +1,10 @@
+require_relative "./concerns/slugifiable.rb"
 class Legend < ActiveRecord::Base
     has_many :legend_stats
     has_many :stats, through: :legend_stats
     has_many :users, through: :stats
+    
+    extend Slugifiable::ClassMethods
+    include Slugifiable::InstanceMethods
     
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+require_relative "./concerns/slugifiable.rb"
 class User < ActiveRecord::Base
     has_many :stats
     has_many :legends, through: :stats
     has_secure_password
+
+    extend Slugifiable::ClassMethods
+    include Slugifiable::InstanceMethods
     
 end
