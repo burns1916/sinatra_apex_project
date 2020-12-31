c8d90eaddc1cd5cd5455bed1eaf9a34d6f9a0a3b
Ryan Burns <ryankburns@gmail.com>
2020-12-31 15:52:12 -0800

    added edit and delete routes to stats controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -15,12 +15,42 @@ class StatsController < ApplicationController
         @stat.legend_id = params[:id]
         @stat.user_id = current_user.id
         @stat.save
-        redirect to "stats/#{@stat.id}/show"
+        redirect to "stats/#{@stat.id}"
     end
 
-    get "/stats/:id/show" do
+    get "/stats/:id" do
         @stat = Stat.find_by_id(params[:id])
         @legend = Legend.find_by_id(@stat.legend_id)
         erb :"stats/show"
+    end
+
+    get "/stats/:id/edit" do
+        @stat = Stat.find_by_id(params[:id])
+        if logged_in? && @stat.user_id == current_user.id
+            erb :"/stats/edit"
+        else
+            redirect to "/login"
+        end
+    end
+
+    patch "/stats/:id" do
+        @stat = Stat.find_by_id(params[:id])
+        if logged_in? && !params[:content].blank? && @stat.user_id == current_user.id
+            @stat.update(kills: params[:kills], wins: params[:wins]) 
+            @stat.save
+            redirect to "/stats/#{@stat.id}"
+        else
+            redirect to "/stats/#{@stat.id}/edit"
+        end
+    end
+
+    delete "/stats/:id/delete" do
+        @stat = Stat.find_by_id(params[:id])
+        if logged_in? && @stat.user_id == current_user.id
+            @stat.delete
+            redirect to "/legends"
+        else
+            redirect to "/login"
+        end
     end
 end
