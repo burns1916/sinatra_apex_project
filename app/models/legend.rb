e59aa543e506860961746b914806f8aee4d7cff5
Ryan Burns <ryankburns@gmail.com>
2021-01-04 21:58:41 -0800

    removed slug methods

--- app/models/legend.rb
+++ app/models/legend.rb
@@ -1,12 +1,6 @@
-require_relative "./concerns/slugifiable.rb"
 class Legend < ActiveRecord::Base
     belongs_to :users
     has_many :stats
 
     enum name: [:Bloodhound, :Gibraltar, :Lifeline, :Pathfinder, :Wraith, :Bangalore, :Caustic, :Mirage, :Octane, :Wattson, :Crypto, :Revenant, :Loba, :Rampart, :Horizon]
-
-    extend Slugifiable::ClassMethods
-    include Slugifiable::InstanceMethods
-
-    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
-require_relative "./concerns/slugifiable.rb"
 class User < ActiveRecord::Base
     has_many :legends
     has_many :stats, through: :legends
     has_secure_password
-
-    extend Slugifiable::ClassMethods
-    include Slugifiable::InstanceMethods
     
 end
